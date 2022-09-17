b78fd119771bd9ffd1c4e0ccdf2174d632095f8b
Sergey Polichnoy <pilatuz@gmail.com>
2022-09-17 21:40:18 +0300

    first early draft version of Span logger

---
+++ span_logger.go
@@ -0,0 +1,97 @@
+package otelzap
+
+import (
+	"context"
+
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// SpanLogger creates ZAP logger which also writes to OpenTelemetry span.
+func SpanLogger(span trace.Span, logger *zap.Logger) *zap.Logger {
+	if span == nil || !span.IsRecording() {
+		return logger // no tracing enabled
+	}
+
+	wrap := func(core zapcore.Core) zapcore.Core {
+		return zapcore.NewTee(core,
+			zapSpanCore{
+				core: core,
+				span: span,
+			})
+	}
+
+	return logger.WithOptions(zap.WrapCore(wrap))
+}
+
+// SpanLoggerFromContext similar to SpanLogger but gets span from context.
+func SpanLoggerFromContext(ctx context.Context, logger *zap.Logger) *zap.Logger {
+	return SpanLogger(trace.SpanFromContext(ctx), logger)
+}
+
+// zapSpanCore writes log entries to the span as OpenTelemetry events.
+type zapSpanCore struct {
+	core zapcore.Core // actually is used to check levels
+	span trace.Span
+	with []zapcore.Field
+}
+
+// Enabled checks if logging level is enabled.
+func (zs zapSpanCore) Enabled(level zapcore.Level) bool {
+	return zs.core.Enabled(level)
+}
+
+// With adds structured context to the Core.
+func (zs zapSpanCore) With(fields []zapcore.Field) zapcore.Core {
+	return zapSpanCore{
+		core: zs.core, // zs.core.With(fields), - so sense yet
+		span: zs.span,
+		with: concatFields(zs.with, fields),
+	}
+}
+
+// Check determines whether the supplied Entry should be logged.
+func (zs zapSpanCore) Check(entry zapcore.Entry, checked *zapcore.CheckedEntry) *zapcore.CheckedEntry {
+	if zs.Enabled(entry.Level) {
+		return checked.AddCore(entry, zs)
+	}
+
+	return checked
+}
+
+// Write serializes the Entry and any Fields supplied at the log site and
+// writes them to OpenTelemetry as an event.
+func (zs zapSpanCore) Write(entry zapcore.Entry, fields []zapcore.Field) error {
+	zs.span.AddEvent(entry.Message,
+		attributesFromZapFields(zs.with, fields,
+			attribute.Stringer("zap.level", entry.Level),
+			attribute.String("zap.logger_name", entry.LoggerName),
+		))
+
+	return nil
+}
+
+// Sync flushes buffered logs.
+func (zs zapSpanCore) Sync() error {
+	return nil // nothing to sync
+}
+
+// concatFields concatenates two set of fields.
+func concatFields(a []zapcore.Field, b []zapcore.Field) []zapcore.Field {
+	if len(a) == 0 {
+		return b // first set is empty, return second one
+	}
+	if len(b) == 0 {
+		return a // second set is empty, return first one
+	}
+
+	// concatenate both non-empty sets
+	out := make([]zapcore.Field, 0, len(a)+len(b))
+	out = append(out, a...)
+	out = append(out, b...)
+
+	return out
+}
---
+++ zap_fields.go
@@ -0,0 +1,252 @@
+package otelzap
+
+import (
+	"encoding"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"math"
+	"reflect"
+	"strconv"
+	"time"
+
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
+	"go.uber.org/zap/zapcore"
+)
+
+// attributesFromZapFields converts multiple ZAP fields into OpenTelemetry WithAttributes() option.
+func attributesFromZapFields(with []zapcore.Field, fields []zapcore.Field, extra ...attribute.KeyValue) trace.EventOption {
+	if len(with)+len(fields) == 0 {
+		// no fields, use extra attributes only
+		return trace.WithAttributes(extra...)
+	}
+
+	// convert each ZAP field
+	attrs := make([]attribute.KeyValue, 0, len(with)+len(fields)+len(extra))
+	attrs = append(attrs, extra...) // use extra "as is"
+	for _, field := range with {
+		attrs = appendZapField(attrs, field)
+	}
+	for _, field := range fields {
+		attrs = appendZapField(attrs, field)
+	}
+
+	return trace.WithAttributes(attrs...)
+}
+
+// appendZapField converts and append ZAP field.
+func appendZapField(attributes []attribute.KeyValue, field zapcore.Field) []attribute.KeyValue {
+	switch field.Type {
+	case zapcore.SkipType, // see zap.Skip()
+		zapcore.NamespaceType: // see zap.Namespace()
+		return attributes // skip it
+
+	case zapcore.BoolType: // see zap.Bool()
+		return append(attributes, attribute.Bool(field.Key, field.Integer != 0))
+
+	case zapcore.Int8Type, // see zap.Int8()
+		zapcore.Int16Type,   // see zap.Int16()
+		zapcore.Int32Type,   // see zap.Int32()
+		zapcore.Int64Type,   // see zap.Int64()
+		zapcore.Uint8Type,   // see zap.Uint8()
+		zapcore.Uint16Type,  // see zap.Uint16()
+		zapcore.Uint32Type,  // see zap.Uint32()
+		zapcore.Uint64Type,  // see zap.Uint64()
+		zapcore.UintptrType: // see zap.Uintptr()
+		return append(attributes, attribute.Int64(field.Key, field.Integer))
+
+	case zapcore.Float32Type: // see zap.Float32()
+		return append(attributes, attribute.Float64(field.Key, float64(math.Float32frombits(uint32(field.Integer)))))
+	case zapcore.Float64Type: // see zap.Float64()
+		return append(attributes, attribute.Float64(field.Key, math.Float64frombits(uint64(field.Integer))))
+
+	case zapcore.Complex64Type: // see zap.Complex64()
+		s := strconv.FormatComplex(complex128(field.Interface.(complex64)), 'E', -1, 64)
+		return append(attributes, attribute.String(field.Key, s))
+	case zapcore.Complex128Type: // see zap.Complex128()
+		s := strconv.FormatComplex(field.Interface.(complex128), 'E', -1, 128)
+		return append(attributes, attribute.String(field.Key, s))
+
+	case zapcore.StringType: // see zap.String()
+		return append(attributes, attribute.String(field.Key, field.String))
+	case zapcore.BinaryType: // see zap.Binary()
+		return append(attributes, attribute.String(field.Key, base64.StdEncoding.EncodeToString(field.Interface.([]byte))))
+	case zapcore.ByteStringType: // see zap.ByteString()
+		return append(attributes, attribute.String(field.Key, string(field.Interface.([]byte))))
+	case zapcore.StringerType: // see zap.Stringer()
+		return append(attributes, attribute.Stringer(field.Key, field.Interface.(fmt.Stringer)))
+
+	case zapcore.DurationType: // see zap.Duration()
+		return append(attributes, attribute.Stringer(field.Key, time.Duration(field.Integer)))
+	case zapcore.TimeType: // see zap.Time()
+		t := time.Unix(0, field.Integer).In(field.Interface.(*time.Location))
+		return append(attributes, attribute.String(field.Key, t.Format(time.RFC3339Nano)))
+	case zapcore.TimeFullType: // see zap.Time()
+		return append(attributes, attribute.String(field.Key, field.Interface.(time.Time).Format(time.RFC3339Nano)))
+
+	case zapcore.ErrorType: // see zap.Error()
+		return append(attributes, attribute.String(field.Key, field.Interface.(error).Error()))
+
+	case zapcore.ReflectType, // see zap.Reflect()
+		zapcore.ArrayMarshalerType,  // see zap.Strings(), zap.Int64s(), ...
+		zapcore.ObjectMarshalerType, // see zap.Object()
+		zapcore.InlineMarshalerType: // see zap.Inline()
+		break // return append(attributes, anyAttr(field.Key, field.Interface))
+	}
+
+	return append(attributes, anyAttr(field.Key, field.Interface))
+}
+
+// anyAttr converts unknown type to OpenTelemetry attribute likely as JSON value.
+func anyAttr(key string, value interface{}) attribute.KeyValue {
+	switch t := value.(type) {
+	case nil:
+		return attribute.String(key, "<nil>")
+
+	case bool:
+		return attribute.Bool(key, t)
+	case []bool:
+		return attribute.BoolSlice(key, t)
+
+	case string:
+		return attribute.String(key, t)
+	case []string:
+		return attribute.StringSlice(key, t)
+	case []byte:
+		return attribute.String(key, base64.StdEncoding.EncodeToString(t))
+
+	case int:
+		return attribute.Int(key, t)
+	case []int:
+		return attribute.IntSlice(key, t)
+
+	case int8:
+		return attribute.Int64(key, int64(t))
+	case int16:
+		return attribute.Int64(key, int64(t))
+	case int32:
+		return attribute.Int64(key, int64(t))
+	case int64:
+		return attribute.Int64(key, t)
+	case []int64:
+		return attribute.Int64Slice(key, t)
+
+	case uint:
+		return attribute.Int64(key, int64(t))
+	case uint8:
+		return attribute.Int64(key, int64(t))
+	case uint16:
+		return attribute.Int64(key, int64(t))
+	case uint32:
+		return attribute.Int64(key, int64(t))
+	case uint64:
+		return attribute.Int64(key, int64(t))
+
+	case float32:
+		return attribute.Float64(key, float64(t))
+	case float64:
+		return attribute.Float64(key, t)
+	case []float64:
+		return attribute.Float64Slice(key, t)
+
+	case encoding.TextMarshaler:
+		if b, err := t.MarshalText(); err == nil {
+			return attribute.String(key, string(b))
+		}
+		// in case of error just try something else below
+	case fmt.Stringer:
+		return attribute.Stringer(key, t)
+	}
+
+	// try reflected value
+	switch rv := reflect.ValueOf(value); rv.Kind() {
+	case reflect.Bool:
+		return attribute.Bool(key, rv.Bool())
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return attribute.Int64(key, rv.Int())
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return attribute.Int64(key, int64(rv.Uint()))
+	case reflect.Float32, reflect.Float64:
+		return attribute.Float64(key, rv.Float())
+	case reflect.String:
+		return attribute.String(key, rv.String())
+
+	case reflect.Slice, reflect.Array:
+		switch rv.Type().Elem().Kind() {
+		case reflect.Bool:
+			return attribute.BoolSlice(key, toBoolSlice(rv))
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			return attribute.Int64Slice(key, toInt64Slice(rv))
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+			return attribute.Int64Slice(key, toUint64Slice(rv))
+		case reflect.Float64:
+			return attribute.Float64Slice(key, toFloat64Slice(rv))
+		case reflect.String:
+			return attribute.StringSlice(key, toStringSlice(rv))
+		}
+	}
+
+	// format as JSON
+	if b, err := json.Marshal(value); err == nil {
+		return attribute.String(key, string(b))
+	}
+
+	// format as %v string as a final option
+	return attribute.String(key, fmt.Sprint(value))
+}
+
+// toBoolSlice converts reflected value to bool slice.
+func toBoolSlice(rv reflect.Value) []bool {
+	N := rv.Len()
+	out := make([]bool, N)
+	for i := 0; i < N; i++ {
+		re := rv.Index(i)
+		out[i] = re.Bool()
+	}
+	return out
+}
+
+// toInt64Slice converts reflected value to int64 slice.
+func toInt64Slice(rv reflect.Value) []int64 {
+	N := rv.Len()
+	out := make([]int64, N)
+	for i := 0; i < N; i++ {
+		re := rv.Index(i)
+		out[i] = re.Int()
+	}
+	return out
+}
+
+// toUint64Slice converts reflected value to int64 slice.
+func toUint64Slice(rv reflect.Value) []int64 {
+	N := rv.Len()
+	out := make([]int64, N)
+	for i := 0; i < N; i++ {
+		re := rv.Index(i)
+		out[i] = int64(re.Uint())
+	}
+	return out
+}
+
+// toFloat64Slice converts reflected value to float64 slice.
+func toFloat64Slice(rv reflect.Value) []float64 {
+	N := rv.Len()
+	out := make([]float64, N)
+	for i := 0; i < N; i++ {
+		re := rv.Index(i)
+		out[i] = re.Float()
+	}
+	return out
+}
+
+// toStringSlice converts reflected value to string slice.
+func toStringSlice(rv reflect.Value) []string {
+	N := rv.Len()
+	out := make([]string, N)
+	for i := 0; i < N; i++ {
+		re := rv.Index(i)
+		out[i] = re.String()
+	}
+	return out
+}

eaa01b91478f28c6daa4c94fce773ffcfcacab68
Sergey Polichnoy <pilatuz@gmail.com>
2025-02-23 12:12:05 +0300

    upgrade dependencies

--- go.mod
+++ go.mod
@@ -1,20 +1,17 @@
 module github.com/Pilatuz/otelzap
 
-go 1.18
+go 1.22.0
 
 require (
-	github.com/golang/mock v1.6.0
-	github.com/stretchr/testify v1.8.1
-	go.opentelemetry.io/otel v1.11.2
-	go.opentelemetry.io/otel/trace v1.11.2
-	go.uber.org/zap v1.24.0
+	github.com/stretchr/testify v1.10.0
+	go.opentelemetry.io/otel v1.34.0
+	go.opentelemetry.io/otel/trace v1.34.0
+	go.uber.org/zap v1.27.0
 )
 
 require (
-	github.com/benbjohnson/clock v1.1.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	go.uber.org/atomic v1.10.0 // indirect
-	go.uber.org/multierr v1.8.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- span_logger_test.go
+++ span_logger_test.go
@@ -4,30 +4,37 @@ import (
 	"context"
 	"testing"
 
-	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
+	"go.opentelemetry.io/otel/trace/noop"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"go.uber.org/zap/zaptest"
 
 	. "github.com/Pilatuz/otelzap"
-	. "github.com/Pilatuz/otelzap/internal/mocked"
 )
 
-//go:generate go run github.com/golang/mock/mockgen@v1.6.0 -mock_names Span=MockedSpan -package mocked -destination internal/mocked/mocked_span.go go.opentelemetry.io/otel/trace Span
+// mockedSpan span for tests.
+type mockedSpan struct {
+	trace.Span
 
+	addEventCb func(string, ...trace.EventOption)
+}
+
+func (mockedSpan) IsRecording() bool { return true }
+func (m mockedSpan) AddEvent(name string, options ...trace.EventOption) {
+	m.addEventCb(name, options...)
+}
+
+// TestSpanLogger unit tests for SpanLogger.
 func TestSpanLogger(t *testing.T) {
 	L1 := zap.NewNop()
 	assert.Same(t, L1, SpanLoggerFromContext(context.Background(), L1))
 
-	ctrl := gomock.NewController(t)
-	span := NewMockedSpan(ctrl)
-	span.EXPECT().
-		IsRecording().
-		Return(true).
-		AnyTimes()
+	span := mockedSpan{
+		Span: noop.Span{},
+	}
 
 	L2, buf2 := newJSONLogger()
 	L2 = L2.Named("my")
@@ -36,15 +43,18 @@ func TestSpanLogger(t *testing.T) {
 		With(zap.String("bar", "hello")).
 		With(zap.Int("baz", 321))
 
-	span.EXPECT().
-		AddEvent("my message",
-			trace.WithAttributes(
+	span.addEventCb = func(name string, options ...trace.EventOption) {
+		cfg := trace.NewEventConfig(options...)
+		assert.Equal(t, "my message", name)
+		assert.Equal(t,
+			[]attribute.KeyValue{
 				attribute.String("zap.level", "info"),
 				attribute.String("zap.logger_name", "my"),
 				attribute.String("bar", "hello"),
 				attribute.Int("baz", 321),
 				attribute.Int("foo", 123),
-			))
+			}, cfg.Attributes())
+	}
 	SL2.Info("my message", zap.Int("foo", 123))
 	SL2.Debug("my message", zap.String("foo", "ignore me"))
 
